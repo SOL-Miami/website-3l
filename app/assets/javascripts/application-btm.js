37932bd164382f27c373f223f8ae63a55db5dd45
Mark Natera <marknatera@gmail.com>
2015-11-29 20:59:20 -0500

    Added theme js and index page html

---
+++ app/assets/javascripts/application-btm.js
@@ -0,0 +1,10 @@
+//= require jquery/jquery
+//= require plugins
+//= require respond
+//= require jquery/jquery.appear
+//= require jquery/jquery.prettyPhoto
+//= require jquery/jquery.flexslider
+//= require jquery/jquery.form-validator
+
+// Theme Initializer
+//= require theme
