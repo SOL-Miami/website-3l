c1b045f1ee51224442fc8882b8e4ff4525ec013d
Hector Gongora <hector@hectorg.net>
2016-01-09 03:27:21 -0500

    7

--- index.php
+++ index.php
@@ -2154,16 +2154,11 @@
                             <li>
                                 <ul class="jx-pricing-wrapper">
                                     <li data-type="standard" class="is-visible">
-                                        
-											<div class="jx-event-slide">                        	
+										<div class="jx-event-slide">                        	
 												<div class="jx-slider-content">
-													<div class="container">
-														<div class="jx-event-box">
 															<div class="jx-event-play">
-																<a href="" data-rel="prettyPhoto"><i class="fa fa-play"></i></a>
+																<a href="http://solmiami.us12.list-manage.com/track/click?u=55b61422e07cc89d7931e62b7&id=d66100828e&e=9d34b359ab" data-rel="prettyPhoto"><i class="fa fa-play"></i></a>
 															</div>
-														</div> 
-													</div>    
 												 </div>
 											</div>        
 									</li>
