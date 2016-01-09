8934a93d4cf07bace419d7e904815da9d6bfcefb
Hector Gongora <hector@hectorg.net>
2016-01-09 03:53:02 -0500

    7

--- index.php
+++ index.php
@@ -2151,16 +2151,21 @@
                         
                        
                        
-                            <li.myclass {display: inline-block;} >
+                           <!-- <li.myclass {display: inline-block;} >-->
+                           <<li>
                                 <ul class="jx-pricing-wrapper">
                                     <li data-type="standard" class="is-visible" >
+										         <div class="one-third columns jx-price-item">   
+										
 										<div class="jx-event-slide">                        	
 												<div class="jx-slider-content">
 															<div class="jx-event-play">
 																<a href="http://solmiami.us12.list-manage.com/track/click?u=55b61422e07cc89d7931e62b7&id=d66100828e&e=9d34b359ab"><i class="fa fa-play"></i></a>
 															</div>
 												 </div>
-											</div>        
+											</div>  
+										       </div>	
+											      
 									</li>
                                 </ul>
                                 <!-- .jx-pricing-wrapper -->
