120c08efa78bf5639bdb86566d3ca36ca6147bb9
Hector Gongora <hector@hectorg.net>
2016-01-22 21:49:39 -0500

    Fixing Video

--- index.php
+++ index.php
@@ -200,7 +200,7 @@
                     	<div class="jx-event-slide">
                             <div class="jx-slider-content">
                             	<div class="container">
-                                    <div class="jx-event-box">
+                                    <div class="jx-event-video">
 									    
                                   <iframe width="1200" height="675" src="https://www.youtube.com/embed/hW097luAbes" frameborder="0" allowfullscreen></iframe>
                                                                             
